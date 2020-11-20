6e35cad12116ca185e94e680b4e7ac4a65bae66a
chenghuaWang <2923277184@qq.com>
2020-11-20 22:45:04 +0800

    update project2.3

--- project2.3/2.3.py
+++ project2.3/2.3.py
@@ -11,11 +11,11 @@ WIN_NAME = "Test show"
 # @color=[r,g,b]
 def my_rectangle(image, pt1, pt2, color):
     for i in range(min(pt1[1], pt2[1]), max(pt1[1], pt2[1])):
-        for j in range(0, 2):
+        for j in range(0, 3):
             image[pt2[0], i, j] = color[j]
             image[pt1[0], i, j] = color[j]
     for i in range(min(pt1[0], pt2[0]), max(pt1[0], pt2[0])):
-        for j in range(0, 2):
+        for j in range(0, 3):
             image[i, pt1[1], j] = color[j]
             image[i, pt2[1], j] = color[j]
 
@@ -43,6 +43,7 @@ class pipin(object):
 def on_mouse_draw(event, x, y, flags, param):
     if mypipin.mouse_move:
         mypipin.reset_image()
+        mypipin.show_image = np.zeros((512, 512, 3), dtype=np.uint8)
     if event == cv2.EVENT_LBUTTONDOWN and mypipin.judge_down == False:
         mypipin.start_pt = (y, x)
         print("START:Y={0},X={1}".format(y, x))
