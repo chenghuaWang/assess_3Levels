12935c03d9d18941332a543a0fc92d8bc806f085
chenghuaWang <2923277184@qq.com>
2020-11-24 18:59:57 +0800

    update project3.0

--- project3.0/3.0.py
+++ project3.0/3.0.py
@@ -1,20 +1,55 @@
-import cv2
 import numpy as np
-import copy
 import pandas as pd
+from scipy import sparse
+from scipy.linalg import lu
+import cv2
 
-A_buffer = pd.read_csv('A.txt', sep=',', header=None, dtype=str, na_filter=False)
-B_buffer = pd.read_csv('B.txt', sep=',', header=None, dtype=str, na_filter=False)
-A_in = np.array(A_buffer).astype(int)
-B_in = np.array(B_buffer).astype(int)
-A_matrix = np.zeros((196608, 196608), dtype=np.uint8)
-B_matrix = np.zeros(196608, dtype=np.uint8)
-print(A_in.shape)
-print(B_in.shape)
+WIN_NAME = 'Test show'
+
+
+def jacobi(a, b, x, k):
+    d_data = a.diagonal()
+    d_inv_buffer = 1 / d_data
+    d = sparse.diags(d_data, dtype=np.int)
+    d_inv = sparse.diags(d_inv_buffer, dtype=np.float)
+    del d_data
+    del d_inv_buffer
+    r = a - d
+    for j in range(0, k + 1):
+        x = d_inv*(b - r * x)
+    return x
+
 
 if __name__ == '__main__':
-    for i in range(0, 804528):
-        A_matrix[A_in[i][0] - 1][A_in[i][1] - 1] = A_in[i][2]
-    for i in range(0, 196608):
-        B_matrix[i] = B_in[i][2]
-
+    A_buffer = pd.read_csv('A.txt', sep=',', header=None, dtype=str, na_filter=False)
+    B_buffer = pd.read_csv('B.txt', sep=',', header=None, dtype=str, na_filter=False)
+    A_matrix = np.array(A_buffer).astype(int)
+    del A_buffer
+    B_matrix = np.array(B_buffer).astype(int)
+    B_matrix = B_matrix[:, 2]
+    del B_buffer
+    A_row = A_matrix[:, 0] - 1
+    A_col = A_matrix[:, 1] - 1
+    A_data = A_matrix[:, 2]
+    sA = sparse.coo_matrix((A_data, (A_row, A_col)), shape=(196608, 196608), dtype=np.int)
+    del A_row
+    del A_col
+    del A_data
+    # print(sA)
+    x0 = np.ones(256 * 256 * 3, dtype=np.float)
+    x0 = jacobi(sA, B_matrix, x0, 1000)
+    print(x0)
+    # print(x0)
+    x0 = x0.astype(int)
+    img = np.zeros((256, 256, 3), dtype=np.uint8)
+    for i in range(0, 256):
+        for j in range(0, 256):
+            img[i][j][2] = x0[i * 256 + j]
+    for i in range(0, 256):
+        for j in range(0, 256):
+            img[i][j][1] = x0[256 * 256 + i * 256 + j]
+    for i in range(0, 256):
+        for j in range(0, 256):
+            img[i][j][0] = x0[256 * 256 * 2 + i * 256 + j]
+    print(x0)
+    cv2.imwrite("D:\AI_ML\pythonProject\project3.0\solver2.png", img)
